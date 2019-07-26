7073f6ff8c227093491fb066a4dc7985fcd24cba
Marat <yeffasol@mail.ru>
2019-07-26 20:21:29 +0300

    Add status code(lesson 9)

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 
 const app = express();
+const products = ["Apple", "Pen", "Computer"];
 
 app.get("/", (reg, res, next) => {
     res.send("it's working");
 })
 
-app.listen(5000,()=>{
+app.get("/products", (reg, res, next) => {
+    res.json({products});
+   console.log("Page ",reg.query.page);
+})
+
+app.get("/products/:id", (reg, res, next) => {
+    if(products[reg.params.id]){
+        res.send(products[reg.params.id]);
+    } else{
+        res.status(404).send("Product not found");
+    }
+})
+
+app.listen(5000, () => {
     console.log("Сервер запустился", new Date());
 })
